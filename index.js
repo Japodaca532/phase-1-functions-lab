3fa1042ff370cf81d0b97eb6a32030cd8ff7253d
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-03-26 16:45:37 -0600

    completedwork.md

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const hq=42
 function distanceFromHqInBlocks(blocks){
 return Math.abs(blocks-hq)
 }
+const block=264
 function distanceFromHqInFeet(feet){
-    return Math.abs(street-feet)
+    return Math.abs(block*feet)
 }
