55ce0b9c523649a6ba38acdd070928f1aba6c170
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-03-26 17:34:28 -0600

    completedwork.md

--- index.js
+++ index.js
@@ -2,7 +2,28 @@ const hq=42
 function distanceFromHqInBlocks(blocks){
 return Math.abs(blocks-hq)
 }
-const feet=264
-function distanceFromHqInFeet(feet){
-    return Math.abs(blocks*feet)
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    const freeRide = 400;
+    let price;
+    if (distance <= freeRide){
+        price = 0;
+    } else if (distance > 400 && distance < 2000){
+        price = (distance - freeRide) * 0.02;
+    } else if (distance > 2000 && distance < 2500){
+        price = 25;
+    } else 
+        price = "cannot travel that far"
+    
+    return price;
+}
+function distanceTravelledInFeet(start, destination){
+    const feetPerBlock = 264;
+    let distance
+    if (start >= destination){
+        distance = (start - destination) * feetPerBlock;
+    } else {
+        distance = (destination - start) * feetPerBlock;
+    }
+    return distance;
 }
